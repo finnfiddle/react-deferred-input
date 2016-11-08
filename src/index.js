c014a925ff5b304fbe7368152221e41cbe6ddf60
Finn Fitzsimons <fitzsimons.finn@gmail.com>
2016-11-08 22:14:24 +0200

    v1

--- src/index.js
+++ src/index.js
@@ -1,137 +1,54 @@
-import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import React from 'react';
+import DeferredInput from 'react-deferred-input';
 
-// keyboard key codes
-const KEY_CODES = {
-  ENTER: 13,
-};
+ReactDOM.render((
+  <DeferredInput
+    value='initial value'
+    onChange={value => alert(value)}
+    focusOnMount={true}
+  />
+), document.getElementById('example-1'));
 
-// same as lodash.omit
-const omit = (obj, keys) => {
-  const result = Object.assign({}, obj);
-  keys.forEach(key => delete result[key]);
-  return result;
-};
+ReactDOM.render((
+  <DeferredInput
+    value='initial value'
+    onChange={value => alert(value)}
+    blurOnEnter={true}
+  />
+), document.getElementById('example-2'));
 
-const noop = () => {};
+ReactDOM.render((
+  <DeferredInput
+    value='initial value'
+    onChange={value => alert(value)}
+    clearOnChange={true}
+  />
+), document.getElementById('example-3'));
 
-// check if value is not null and not undefined
-const isSet = val => val !== null && typeof val !== 'undefined';
+ReactDOM.render((
+  <DeferredInput
+    value='initial value'
+    onChange={value => alert(value)}
+    inputComponent='textarea'
+  />
+), document.getElementById('example-4'));
 
-class DeferredInput extends Component {
-
-  constructor() {
-    super();
-    // initial state
-    this.state = { value: '' };
-  }
-
-  componentWillMount() {
-    // update state with props
-    this.setState({ value: this.props.value || '' });
-  }
-
-  componentDidMount() {
-    // focus on element when it mounts if `props.focusOnMount` is set to true
-    if (this.props.focusOnMount) {
-      setTimeout(() => this.focus.call(this), 500);
-    }
-  }
-
-  componentWillReceiveProps(nextProps) {
-    // update value if new value prop is received
-    if (this.props.value !== nextProps.value) {
-      this.setState({ value: nextProps.value });
-    }
-  }
-
-  // internal input `onChange` handler
-  handleChange(event) {
-    this.setState({ value: event.target.value });
-  }
-
-  // `onBlur` handler
-  handleBlur() {
-    // call `props.onBlur` if set
-    this.props.onBlur(this.state.value);
-    // call `props.onChange` of provided and value is changed
-    if (isSet(this.props.onChange) && this.state.value !== this.props.value) {
-      this.props.onChange(this.state.value);
-    }
-    // if `props.clearOnChange` is true then clear the input when blurred
-    if (this.props.clearOnChange) this.setState({ value: '' });
-  }
-
-  // get props for input where `onChange`, `onBlur` and `value` are replaced with private methods
-  getChildProps() {
-    const privateProps = [
-      'value',
-      'onChange',
-      'onBlur',
-      'isInput',
-      'onKeyDown',
-      'blurOnEnter',
-      'focusOnMount',
-      'clearOnChange',
-      'inputComponent',
-    ];
-
-    return Object.assign({}, omit(this.props, privateProps), {
-      value: isSet(this.state.value) ? this.state.value : '',
-      onChange: this.handleChange.bind(this),
-      onBlur: this.handleBlur.bind(this),
-      onKeyDown: this.handleKeyDown.bind(this),
-    });
-  }
-
-  // trigger the specified action on the first textarea or input found within an element
-  // (or the element itself)
-  callMethodOnElementOrChild(element, action) {
-    if (['TEXTAREA', 'INPUT'].indexOf(element.tagName) > -1) {
-      return element[action]();
-    } else if (element.childNodes.length) {
-      [].forEach.call(element.childNodes, child => this.callMethodOnElementOrChild(child, action));
-    }
-  }
-
-  // trigger blur on element
-  blur() {
-    this.callMethodOnElementOrChild(this.input, 'blur');
-  }
-
-  // trigger focus on element
-  focus() {
-    this.callMethodOnElementOrChild(this.input, 'focus');
-  }
-
-  // key down event handler
-  handleKeyDown(event) {
-    // if `props.blurOnEnter` and key pressed is ENTER then call blur()
-    if (this.props.blurOnEnter && event.keyCode === KEY_CODES.ENTER && !event.shiftKey) {
-      this.blur.call(this);
-    }
-    if (this.props.onKeyDown) this.props.onKeyDown(event);
-  }
-
+class CustomInput extends React.Component {
   render() {
     return (
-      <this.props.inputComponent
-        {...this.getChildProps()}
-        ref={
-          c => {
-            this.input = c;
-          }
-        }
+      <input
+        style={{ borderColor: 'cyan' }}
+        {...this.props}
       />
     );
   }
 }
 
-DeferredInput.defaultProps = {
-  blurOnEnter: false,
-  focusOnMount: false,
-  onBlur: noop,
-  clearOnChange: false,
-  inputComponent: 'input',
-};
-
-export default DeferredInput;
+ReactDOM.render((
+  <DeferredInput
+    value='initial value'
+    onChange={value => alert(value)}
+    inputComponent={CustomInput}
+  />
+), document.getElementById('example-5'));
